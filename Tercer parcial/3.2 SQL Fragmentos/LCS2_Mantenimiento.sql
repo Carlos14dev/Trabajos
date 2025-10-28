27c70877f93ad617426b1b6b2c92e47d4b5556e5
Carlos14dev <mo423308@uaeh.edu.mx>
2025-10-27 20:32:49 -0600

    Create LCS2_Mantenimiento database and tables

---
+++ Tercer parcial/3.2 SQL Fragmentos/LCS2_Mantenimiento.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE LCS2_Mantenimiento;
+USE LCS2_Mantenimiento;
+
+CREATE TABLE vehiculo (
+  id_auto INT NOT NULL,
+  marca VARCHAR(255) NOT NULL,
+  modelo VARCHAR(255) NOT NULL,
+  anio INT NOT NULL,
+  color VARCHAR(255) NOT NULL,
+  placa VARCHAR(255) NOT NULL,
+  PRIMARY KEY(id_auto)
+);
+
+CREATE TABLE mantenimiento (
+  id_mantenimiento INT NOT NULL,
+  id_auto INT NOT NULL,
+  fechainicio DATE NOT NULL,
+  fechafinal DATE NOT NULL,
+  diagnostico VARCHAR(255) NOT NULL,
+  descripcion VARCHAR(500) NOT NULL,
+  PRIMARY KEY(id_mantenimiento),
+  FOREIGN KEY (id_auto) REFERENCES vehiculo(id_auto)
+);
